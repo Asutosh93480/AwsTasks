6737463a3c45f8c39c9b5f0bf7f72deb733f3ed1
asutosh_panda <asutosh_panda@epam.com>
2025-03-11 10:58:43 +0530

    Initial commit

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -3,42 +3,50 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.lambda.url.AuthType;
-
 import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
-    lambdaName = "hello_world",
-	roleName = "hello_world-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+		lambdaName = "hello_world",
+		roleName = "hello_world-role",
+		isPublishVersion = true,
+		aliasName = "${lambdas_alias_name}",
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 
 @LambdaUrlConfig(authType = AuthType.NONE)
 
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+public class HelloWorld implements RequestHandler<Map<String , Object>, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		String method = context.getAwsRequestId();
-		if(method!=null) {
-			System.out.println("Hello from lambda");
-			Map<String, Object> resultInsideMap = new HashMap<String, Object>();
-			resultInsideMap.put("statusCode", 200);
-			resultInsideMap.put("message", "Hello from Lambda");
-			Map<String, Object> resultMap = new HashMap<String, Object>();
-			resultMap.put("statusCode", 200);
-			resultMap.put("body", resultInsideMap);
-			return resultMap;
+	public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
+		System.out.println("Hello from lambda");
+		String path = (String) event.getOrDefault("rawPath", event.get("path"));
+		Map<String, Object> requestContext = (Map<String, Object>) event.get("requestContext");
+		Map<String, Object> http = requestContext != null ? (Map<String, Object>) requestContext.get("http") : null;
+		String method = http != null ? (String) http.get("method") : (String) event.get("httpMethod");
+		if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+			return createSuccessResponse();
+		} else {
+			return createErrorResponse("Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " + method);
 		}
-		else{
-			Map<String, Object> errorResponse = new HashMap<>();
-			errorResponse.put("statusCode", 400);
-			errorResponse.put("message", "Bad request syntax or unsupported method. Request path: /cmtr-84c6fb31. HTTP method: GET");
-			return errorResponse;
-		}
+	}
+
+	private Map<String, Object> createSuccessResponse() {
+		Map<String, Object> response = new HashMap<>();
+		response.put("statusCode", 200);
+		response.put("headers", Map.of("Content-Type", "application/json"));
+		response.put("body", "{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
+		return response;
+	}
+
+	private Map<String, Object> createErrorResponse(String message) {
+		Map<String, Object> response = new HashMap<>();
+		response.put("statusCode", 400);
+		response.put("headers", Map.of("Content-Type", "application/json"));
+		response.put("body", String.format("{\"statusCode\": %d, \"message\": \"%s\"}", 400, message));
+		return response;
 	}
 }
